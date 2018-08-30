d8d46a1fd89f308772f1fd80b427534aa90e0ad3
Kurorido <kurorido1917@gmail.com>
2018-08-30 21:59:23 +0800

    'v3'

--- index.js
+++ index.js
@@ -21,33 +21,7 @@ var server = app.listen(process.env.PORT || 8080, function() {
 });
 
 function handleEvent(event) {
-	var source = event.source;
-  	switch (event.message.type) {
-	  case 'text':
-	    switch (source.type) {
-	      case 'user':
-	      return client.getProfile(source.userId).then(function(profile) {
-	        return client.replyMessage(event.replyToken, {
-	          type: 'text',
-	          text: ('你的名字是: '+profile.displayName+'，你的狀態是: '+profile.statusMessage)
-	        });
-	      });
-	      case 'group':
-	        return client.getGroupMemberProfile(source.groupId,    source.userId).then(function(profile) {
-	          return client.replyMessage(event.replyToken, {
-	            type: 'text',
-	            text: ('你的名字是: '+profile.displayName+'，你的狀態是: '+profile.statusMessage)
-	          });
-	        });
-	      case 'room':
-	        return client.getRoomMemberProfile(source.roomId, source.userId).then(function(profile) {
-	          return client.replyMessage(event.replyToken, {
-	            type: 'text',
-	            text: ('你的名字是: '+profile.displayName+'，你的狀態是: '+profile.statusMessage)
-	          });
-	        });
-	    }
-	}
+	
 }
 
 var timer2;
