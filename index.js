8b78dd6734dddfbedf78090698a8a87f8b355550
Kurorido <kurorido1917@gmail.com>
2018-08-30 21:31:55 +0800

    First commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 const lineConfig = {
-  channelAccessToken: 'Srnv8nMZG0QnSKkjc8VwgcsKle9hWXGGYfSQ/oX76z7wU18V98bWJhZCClWnahe1UH6DlRbesy+4qpG7xCZ+cr5pjSa87SZ3uY/nocttPeqj8rgPCnfq2lc2X60sv98xsouC28WckEKHE8KRC35+vgdB04t89/1O/w1cDnyilFU=',
-  channelSecret: 'b45811e9c4fd332a208a99a8a7e68d63'
+  channelAccessToken: process.env.HEROKU_LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.HEROKU_LINE_CHANNEL_SECRET
 };
 const client = new line.Client(lineConfig);
 const app = express();
