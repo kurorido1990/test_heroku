9de059195726ee984144de557d270f52d406a1f8
Kurorido <kurorido1917@gmail.com>
2018-08-30 22:40:49 +0800

    First commit

--- index.js
+++ index.js
@@ -65,13 +65,9 @@ var timer2;
 function test() {
 
 console.log("xxxxxx");
-   client.pushMessage('Ca235f9483eb71d8f7a381b2777011c17', json({
+   client.pushMessage('Ca235f9483eb71d8f7a381b2777011c17', {
   type: 'text',
   text: 'Hello World!'
-})).then(function(){
-	console.log("not xxxxx");
-}).catch((error) => {
-  console.log(error);
 });
 
 console.log("xx333333");
