ba790e56538515bf8607f510f24e25500f651212
Kurorido <kurorido@85.LAN-11.10F.oa.vgs.tw>
2018-08-31 10:45:40 +0800

    test

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ function checkbrocast() {
 						 var hour = element[2].split(":");
 						 console.log("day : " + element[1] + " hour: " + hour[0]);
 
-						 if ($element[1] == time.day && hour[0] < time.hour && g_hour > hour[0]){
+						 if (element[1] == time.day && hour[0] < time.hour && g_hour > hour[0]){
 							 test(element[0]);
 						 }
      		}
