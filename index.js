f0e1f36cbbfbc5ab34192ee5d14904bcd38a8a97
Kurorido <kurorido@179.LAN-11.10F.oa.vgs.tw>
2018-09-13 18:15:15 +0800

    test

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const lineConfig = {
 };
 const client = new line.Client(lineConfig);
 const app = express();
-
+ 
 var myClientSecret={"installed":{"client_id":"129892641691-a6mhvuh0lbabhimaffjie822sld2eboo.apps.googleusercontent.com","project_id":"cedar-hawk-214915","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://www.googleapis.com/oauth2/v3/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"YdqOC0e_VNksq-cExF6M8mYi","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
 var auth = new googleAuth();
 var oauth2Client = new auth.OAuth2(myClientSecret.installed.client_id,myClientSecret.installed.client_secret, myClientSecret.installed.redirect_uris[0]);
